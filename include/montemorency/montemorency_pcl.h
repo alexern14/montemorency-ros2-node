5b9846561427f90f406f107d1968c56a54bbd39f
Alex <alex.m.ernst14@gmail.com>
2024-04-12 15:35:29 -0600

    initial tree segmentation on montemorency dataset

---
+++ include/montemorency/montemorency_pcl.h
@@ -0,0 +1,47 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include <std_msgs/msg/int8.hpp>
+
+class Montemorency_Trees : public rclcpp::Node {
+public:
+  /**
+   * @brief A constructor for Montemorency_Trees class
+   * @param options Additional options to control creation of the node.
+   */
+  explicit Montemorency_Trees(
+      const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
+
+  /**
+   * @brief A destructor for Montemorency_Trees class
+   */
+  ~Montemorency_Trees(){};
+
+protected:
+  /**
+   * @brief Use a no filter of pcl library
+   * @param msg Pointcloud2 message receveived from the ros2 node
+   * @return -
+   * @details Omit pointcloud filtering in this example
+   */
+  void topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+
+  // ROS2 subscriber and related topic name
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
+  std::string param_topic_pointcloud_in;
+
+  // ROS2 publisher and related topic name
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
+  std::string param_topic_pointcloud_out;
+
+  // ROS2 publisher and related topic name
+  rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr
+      tree_count_publisher;
+  std::string param_tree_count;
+
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr
+      publisherFiltered_;
+  std::string param_topic_pointcloud_filtered_out;
+
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisherPlane_;
+  std::string param_topic_pointcloud_filtered_out_plane;
+};
